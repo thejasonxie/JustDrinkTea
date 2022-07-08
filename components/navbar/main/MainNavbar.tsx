2155b00e2c250128847a7ac864900a3650b7b137
Jason Xie <thejasonxie@gmail.com>
2022-07-07 22:16:24 -0400

    feat: added navbar logo

--- components/navbar/main/MainNavbar.tsx
+++ components/navbar/main/MainNavbar.tsx
@@ -1,14 +1,32 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './MainNavbar.module.css';
+
 export interface IMainNavbar {}
 
 const MainNavbar: React.FC<IMainNavbar> = ({}) => {
   return (
-    <div className="flex flex-row justify-between">
-      <section>Logo</section>
+    <nav className="flex flex-row justify-between">
+      <section>
+        <Link href="/">
+          <a>
+            <div className="flex flex-row items-center">
+              <Image
+                src="/images/company/logo.png"
+                alt="Just Drink Tea Logo"
+                height="98"
+                width="165"
+              />
+              <span className={styles.logotext}>Just Drink Tea</span>
+            </div>
+          </a>
+        </Link>
+      </section>
       <section className="flex flex-row">
         <div>Menu</div>
         <div>User Section</div>
       </section>
-    </div>
+    </nav>
   );
 };
 
